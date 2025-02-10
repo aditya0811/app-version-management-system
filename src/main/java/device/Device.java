e01eb04df3a4f4c5985402f281fe8cc13c24f6c1
Aditya Mahajan <aditya.m.1997@gmail.com>
2025-02-10 15:06:27 +0530

    devices update

--- src/main/java/device/Device.java
+++ src/main/java/device/Device.java
@@ -1,5 +1,6 @@
 package device;
 
+import application.Application;
 import application.ApplicationFile;
 import java.util.HashMap;
 import java.util.Map;
@@ -30,6 +31,26 @@ public class Device {
 
   public void setApplicationsInstalled(Map<String, ApplicationFile> applicationsInstalled) {
     this.applicationsInstalled = applicationsInstalled;
+  }
+
+  public void installApplicationInTheDevice(Application application) {
+    //may be add validation, app already installed
+    if (applicationsInstalled.get(application.getApplicationId()) != null) {
+      //throw exception, installing duplicate app
+    } else {
+      String latestVersion = application.getLatestVersion();
+      ApplicationFile latestApplicationFile = application.getApplicationFileVersionMap().get(latestVersion);
+      applicationsInstalled.put(application.getApplicationId(), latestApplicationFile);
+      System.out.println("The device with id: " + deviceId + " is installed with application :" +
+          application.getApplicationId() + ". It is with latest version : " + application.getLatestVersion() +
+          ". Its content are : " + latestApplicationFile.getMetadata());
+
+      // Update application as well with the device
+      application.getDevicesMap().put(deviceId, this);
+      System.out.println("The application :" + application.getApplicationId() + " is added to device :" +
+          deviceId + ". Total applications installed in device : " + applicationsInstalled.size() +
+          ". Total devices with this application: " + application.getDevicesMap().size());
+    }
   }
 
 
